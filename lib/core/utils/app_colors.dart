e4e8753cc3647792708fd5c2f678424f4351ce7d
Nader <ns1568@fayoum.edu.eg>
2024-02-18 21:03:44 +0200

    Add AppColors class with color constants

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const primaryColor = Color(0xffed820e);
+  static const backgroundColor = Color(0xffffffff);
+  static const secondBackgroundColor = Color(0xfffbfaf4);
+  static const mainTextColor = Color(0xff000000);
+  static const hintTextColor = Color(0xff898888);
+  static const buttonTextColor = Color(0xfffffefe);
+  static const chatTextColor = Color(0xff797272);
+  static const buttonBackgroundColor = Color(0xffECE9E9);
+  static const imageBackgroundColor = Color(0xffd9d9d9);
+  static const shadowColor = Color(0x40000000);
+}
