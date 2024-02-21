5183cdb1a7ad758197b24110a4aa2e90cb5cec50
Nader <ns1568@fayoum.edu.eg>
2024-02-21 18:22:21 +0200

    Refactor sign up view body widget

--- lib/features/Auth/presentation/views/widgets/sign_up widgets/sign_up_view_body.dart
+++ lib/features/Auth/presentation/views/widgets/sign_up widgets/sign_up_view_body.dart
@@ -1,10 +1,43 @@
+import 'package:driver/core/utils/app_routes.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import '../custom_another_option_row.dart';
+import '../custom_google_sign_in_button.dart';
+import 'custom_sign_up_appbar.dart';
+import 'custom_sign_up_form.dart';
 
 class SignUpViewBody extends StatelessWidget {
   const SignUpViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Column(children: [
+        const CustomSignUpAppbar(),
+        const SizedBox(height: 54),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(56, 0, 52, 29),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const CustomSignUpForm(),
+              const Gap(38),
+              CustomGoogleSignInButton(
+                buttonText: 'Sign in With Google',
+                onPressed: () {},
+              ),
+              const Gap(19),
+              CustomAnotherOptionRow(
+                messageText: 'already have an account? ',
+                actionText: 'Sign in',
+                onTap: () {
+                  AppRoutes.router.push(AppRoutes.signInView);
+                },
+              ),
+            ],
+          ),
+        ),
+      ]),
+    );
   }
 }
